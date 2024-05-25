32b76c8b7ceea249465ed89350d3ff71f462cd66
mateus-hub <theusskt2018@gmail.com>
2024-05-24 21:34:13 -0300

    Tratando excecao com resposta customizada

--- backend/src/main/java/com/devsuperior/dscommerce/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dscommerce/services/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 
 	public ResourceNotFoundException(String msg) {
 		super(msg);
